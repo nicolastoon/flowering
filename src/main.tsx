8869acc19b255a3085a50ffe2ee7a8f0be3f6a21
nicolas toon <nicolastoon05@gmail.com>
2025-08-05 17:28:31 -0700

    update image filepaths

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
+  </StrictMode>,
 )
